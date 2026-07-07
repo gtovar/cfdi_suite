a3bcb21c54caee53692619c17ea300f8065a36b6
Gilberto Tovar <tovarninogilberto@gmail.com>
2026-07-07 17:20:38 -0600

    fix: definir pool de conexiones controlado en router de batch

--- backend/app/routers/batch.py
+++ backend/app/routers/batch.py
@@ -21,7 +21,7 @@ REDIS_HOST = os.getenv("REDIS_HOST", "localhost")
 REDIS_PORT = int(os.getenv("REDIS_PORT", 6379))
 REDIS_PASSWORD = os.getenv("REDIS_PASSWORD", None)
 
-pool = redis.Redis(
+pool = redis.ConnectionPool(
     host=REDIS_HOST, 
     port=REDIS_PORT, 
     password=REDIS_PASSWORD, 
