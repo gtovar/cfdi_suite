dffcec4330d3baab4968b526376a60962d7b148c
Gilberto Tovar <tovarninogilberto@gmail.com>
2026-07-09 12:51:01 -0600

    print all variables

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -4,6 +4,8 @@ import {MotionConfig} from 'motion/react';
 import App from './App.tsx';
 import './index.css';
 
+console.log("📡 TODAS LAS VARIABLES VISIBLES POR VITE:", (import.meta as any).env);
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <MotionConfig reducedMotion="user">
